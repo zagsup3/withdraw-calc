7e015058c3b4223cf43fb9c48427f8ec6b2825d7
Rishabh Panwar <rishabh.panwar@neosoftmail.com>
2025-05-15 00:33:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     output: "export",
     images: { unoptimized: true },
     trailingSlash: true,
+    basePath: "/withdraw-calc",
 };
 
 export default nextConfig;
